819385f8968b736efa2049f9d2403fc866c9dd8f
Jasurbek Ruziboyev <j.roziboyev@newuzbekistanuniversity.uz>
2023-03-31 12:21:02 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    // int a,b;
+    //   cout << "Enter a number between 0 and 1000: ";
+    // cin >> a;
+    // cout << a / 100 +a / 10 - 10 * (a / 100) + a - 10 * ( a / 10 ) << endl;
+    // float a,b,c;
+    // cout << "Enter v0, v1, and t:     " << endl;
+    // cin >> a >> b >> c;
+    // cout << "The average acceleration is " << ( b - a ) / c << endl;
+    // float a , b;
+    // cout << "Enter speed and acceleration: ";
+     // cin >> a >> b;
+    // cout << "The minimum runway length for this airplane is " << a * a / (2*b) << endl;
+   // float a , b ;
+     //cout << "Enter the length of the side: " ;
+    //cin >> a ;
+    //b = 3 * sqrt(3) / 2 * a * a ;
+     // cout << "The area of the hexagon is " << b << endl;
+     return 0;
+}
